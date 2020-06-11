808e821edd9b00f62177a395374f55e007db9c28
BigForceGun <BigForceGun@gmail.com>
2020-06-11 16:39:17 +0300

    big refuq

--- .zsh/antibody.zsh
+++ .zsh/antibody.zsh
@@ -1,33 +1,28 @@
-source <(antibody init)
-
-
 antibody bundle <<EOB
   sorin-ionescu/prezto
-  sorin-ionescu/prezto folder:modules/utility
-  sorin-ionescu/prezto folder:modules/completion
-  robbyrussell/oh-my-zsh path:plugins/encode64 
 
   robbyrussell/oh-my-zsh path:plugins/fancy-ctrl-z
-  robbyrussell/oh-my-zsh path:plugins/rust
-  robbyrussell/oh-my-zsh path:plugins/ruby
-  robbyrussell/oh-my-zsh path:plugins/rvm
-  robbyrussell/oh-my-zsh path:plugins/systemd
-  robbyrussell/oh-my-zsh path:plugins/redis-cli
-  robbyrussell/oh-my-zsh path:plugins/git-flow
-  robbyrussell/oh-my-zsh path:plugins/docker
+
+
   hlissner/zsh-autopair
   marzocchi/zsh-notify
-  rupa/z
-  changyuheng/fz
   Tarrasch/zsh-bd
   zsh-users/zsh-syntax-highlighting
-  zsh-users/zsh-history-substring-search
-  zsh-users/zsh-completions
+
+  zsh-users/zsh-autosuggestions
   zsh-users/zaw
-  mafredri/zsh-async
-  
   denysdovhan/spaceship-prompt
 EOB
+
+# robbyrussell/oh-my-zsh path:plugins/encode64
+# robbyrussell/oh-my-zsh path:plugins/rust
+# robbyrussell/oh-my-zsh path:plugins/ruby
+# robbyrussell/oh-my-zsh path:plugins/rvm
+# zsh-users/zsh-history-substring-search
+# robbyrussell/oh-my-zsh path:plugins/systemd
+#  robbyrussell/oh-my-zsh path:plugins/redis-cli
+#  robbyrussell/oh-my-zsh path:plugins/git-flow
+#  robbyrussell/oh-my-zsh path:plugins/docker
 # soimort/translate-shell
 # robbyrussell/oh-my-zsh path:plugins/capistrano
 # aperezdc/zsh-fzy
@@ -37,7 +32,5 @@ EOB
 # robbyrussell/oh-my-zsh path:plugins/cargo
 # robbyrussell/oh-my-zsh path:plugins/bundler
 
-#antigen theme maximbaz/spaceship-prompt
 #antibody bundle romkatv/powerlevel10k
 
-#antibody theme denysdovhan/spaceship-prompt
--- .zsh/fuzzy.zsh
+++ .zsh/fuzzy.zsh
@@ -1,50 +1,46 @@
-# тут сейчас два фуззи файндера
-# - fzf
-# - fzy
-# я пока выбираю FZF, потому что
-# - он асихронный, FZY на больших списках встревает секунд на 5, а FZF сразу начинает искать #TODO придумать как сделать так же в FZY
-# - FZY не уммет в курсор в виджете - ставит непонятные символы
-# В будущем надо переехать на FZY потому что он есть в репах, и по поиску круче
+export FZF_COMPLETION_TRIGGER=''
 
+FZF_MODE="FD"
 
-FUZZY_MODE="fzf"
+export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --ansi"
 
-if [[ ${FUZZY_MODE} = "fzf" ]]; then
-
-  # [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
-  [[ -f /usr/share/fzf/completion.zsh ]] && source /usr/share/fzf/completion.zsh
-  [[ -f /usr/share/fzf/key-bindings.zsh ]] && source /usr/share/fzf/key-bindings.zsh
-
-  # Better FZF
-  export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --ansi"
-  # export FZF_DEFAULT_COMMAND="fd --type file --color=always"
+if [[ ${FZF_MODE} = "AG" ]]; then
   export FZF_DEFAULT_COMMAND='ag --hidden  -g""'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-  export FZF_ALT_C_COMMAND_EXCLUSIONS=$(sed -e '/^$/,$d' ~/.agignore | while read -r line; do printf "-path '*/%s' -o " "${line:0:-1}"; done | sed 's/ -o $//')
+  export FZF_ALT_C_COMMAND_EXCLUSIONS=$(sed -e '/^$/,$d' ~/.ignore | while read -r line; do printf "-path '*/%s' -o " "${line:0:-1}"; done | sed 's/ -o $//')
   export FZF_ALT_C_COMMAND="command find -L ~ \( $FZF_ALT_C_COMMAND_EXCLUSIONS \) -prune -o -type d -print 2> /dev/null | sed 1d"
 
   _fzf_compgen_path() {
     ag -g "" "$1"
   }
 
-  export FZF_COMPLETION_TRIGGER=''
-  bindkey '^T' fzf-completion
-  bindkey '^I' $fzf_default_completion
+elif [[ ${FZF_MODE} = "FD" ]]; then
+  export FZF_DEFAULT_COMMAND='fd --hidden --follow --type=f --color=always'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-elif [[ ${FUZZY_MODE} = "fzy" ]]; then
-
-  bindkey '\ec' fzy-cd-widget
-  bindkey '^T'  fzy-file-widget
-  bindkey '^R'  fzy-history-widget
-  bindkey '^P'  fzy-proc-widget
-
-  fzy-file-widget-list-files() { rg --files --hidden --smart-case 2>/dev/null }
-  zstyle :fzy:file command fzy-file-widget-list-files
-  zstyle :fzy:tmux    enabled      no
-
-  __fz_filter() { fzy }
+  _fzf_compgen_path() { fd --hidden --follow --type=f . "$1" }
+  # Default directory search commands
+  export FZF_ALT_C_COMMAND='fd --hidden --follow --type=d --color=always'
+  _fzf_compgen_dir() { fd --hidden --follow --type=d . "$1" }
 fi
 
 
+. /usr/share/fzf/completion.zsh
+. /usr/share/fzf/key-bindings.zsh
+
+#bindkey '^T' fzf-completion
+#bindkey '^I' $fzf_default_completion
+
+#bindkey -r '^[c'
+
+bindkey '\t' expand-or-complete
+
+#export FZ_CMD=j
+#export FZ_SUBDIR_CMD=jj
+
+antibody bundle <<EOB
+  rupa/z
+  changyuheng/fz
+EOB
 
 
