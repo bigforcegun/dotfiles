8bfc458b2d7fcdfb12b910ed160c0915a780af5c
BigForceGun <BigForceGun@gmail.com>
2020-07-21 22:58:40 +0300

    fix zsh notify config

--- .zsh/zsh-notify.zsh
+++ .zsh/zsh-notify.zsh
@@ -3,3 +3,5 @@ zstyle ':notify:*' error-title 'Error'
 zstyle ':notify:*' success-title 'Success'
 zstyle ':notify:*' error-icon '/usr/share/icons/Papirus-Dark/64x64/categories/system-error.svg' # KDE
 zstyle ':notify:*' success-icon '/usr/share/icons/Papirus-Dark/64x64/categories/hwinfo.svg' # KDE
+zstyle ':notify:*' expire-time 10000
+zstyle ':notify:*' enable-on-ssh yes
