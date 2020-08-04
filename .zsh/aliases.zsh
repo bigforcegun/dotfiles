e174e0a19005865f7182b2e5263d677ae7bcc090
BigForceGun <BigForceGun@gmail.com>
2020-08-04 23:22:47 +0300

    add dragon

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -47,6 +47,8 @@ alias c11="awk '{print \$11}'"
 
 alias print_dpi="xdpyinfo | grep dots"
 alias nstat='stat -c "%a %n"'
+alias dragon='dragon-drag-and-drop'
+  
 
 alias prm=". /usr/local/bin/prm.sh"
 
