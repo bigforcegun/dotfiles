28a4b632e5d8a8cd8993157bd633e80d5bb2b0b5
BigForceGun <bigforcegun@pm.me>
2025-08-13 15:44:51 +0700

    vscode terminal tmux fixes

--- .zsh/autorun-tmux.zsh
+++ .zsh/autorun-tmux.zsh
@@ -1,4 +1,17 @@
+# https://rakhesh.com/linux-bsd/vscode-vscodium-unable-to-resolve-your-shell-environment/
+if [[ -n "$VSCODE_RESOLVING_ENVIRONMENT" ]]; then
+  return
+fi
+
 if [[ -z "$TMUX" ]]; then
-  # todo - добавить название программы которая вызвала шелл
-  exec tmux new
+
+  if [[ -n "$VSCODE_SHELL_INTEGRATION" || "$TERM_PROGRAM" == "vscode" || "$TERM_PROGRAM" == "cursor" ]]; then
+    # шелл без тмукса
+    # return
+    exec tmux new -A -s vscode -n vscode \; new-window
+  else
+    # todo - добавить название программы которая вызвала шелл
+    exec tmux new
+  fi
+
 fi
