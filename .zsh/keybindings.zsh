31bf352bf817e6ef725edc7b5f71b32bdcd23cba
BigForceGun <bigforcegun@pm.me>
2026-06-27 23:24:57 +0700

    kb fix

--- .zsh/keybindings.zsh
+++ .zsh/keybindings.zsh
@@ -1,7 +1,7 @@
 bindkey -M emacs "$key_info[Up]" history-substring-search-up
 bindkey -M emacs "$key_info[Down]" history-substring-search-down
-bindkey -M emacs "$key_info[Control]K" backward-kill-line
-bindkey -M emacs "$key_info[Control]V$key_info[Control]V" edit-command-line
-bindkey -M emacs "^_" pound-toggle
+#bindkey -M emacs "$key_info[Control]K" backward-kill-line
+#bindkey -M emacs "$key_info[Control]V$key_info[Control]V" edit-command-line
+#bindkey -M emacs "^_" pound-toggle
 bindkey -M emacs "\e[1;3D" backward-word
 bindkey -M emacs "\e[1;3C" forward-word
